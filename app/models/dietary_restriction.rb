8f709ec92e126a80306ba36011f606ecec425906
Darcy Van Buskirk <darcyvanbuskirk@hntvlan569.2749.wlan.wireless-resnet.upenn.edu>
2016-04-28 18:12:41 -0400

    fix dietary_restrictions model.

--- app/models/dietary_restriction.rb
+++ app/models/dietary_restriction.rb
@@ -2,6 +2,4 @@
 class DietaryRestriction < ActiveRecord::Base
   belongs_to :user
   belongs_to :dish
-  attributes :vegan, :vegetarian, :gluten, :lactose, :egg, :fish, :shellfish
-  attributes :peanuts, :tree_nuts, :soy, :kosher, :halal, :notes
 end
