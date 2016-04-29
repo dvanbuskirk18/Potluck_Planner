eb06cbdfc55202967d882e9c403fb6620d630b6b
Darcy Van Buskirk <darcy.vanbuskirk@gmail.com>
2016-04-28 22:35:26 -0400

    Add integer cast to column_change migration.

--- db/migrate/20160425225034_change_quantity_in_dishes.rb
+++ db/migrate/20160425225034_change_quantity_in_dishes.rb
@@ -1,5 +1,5 @@
 class ChangeQuantityInDishes < ActiveRecord::Migration
   def change
-    change_column :dishes, :quantity, :integer
+    change_column :dishes, :quantity, :integer, 'integer USING CAST(quantity AS integer)'
   end
 end
