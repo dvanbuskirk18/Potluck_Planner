b19d895421f3e0f7d13814294ed4cbc0f2d92406
Darcy Van Buskirk <darcy.vanbuskirk@gmail.com>
2016-04-28 22:43:58 -0400

    Add integer cast to servings column in dishes table.

--- db/migrate/20160425225716_change_servings_in_dishes.rb
+++ db/migrate/20160425225716_change_servings_in_dishes.rb
@@ -1,5 +1,5 @@
 class ChangeServingsInDishes < ActiveRecord::Migration
   def change
-    change_column :dishes, :servings, :integer
+    change_column :dishes, :servings, :integer, 'integer USING CAST(servinds AS integer)'
   end
 end
